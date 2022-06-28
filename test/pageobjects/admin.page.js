d999913e9b2544fc1e4fd3257744bad132b7338b
Martin Rova <tincho.rova@gmail.com>
2022-06-28 12:50:07 -0300

    TG-QA: added edit and delete tests

--- test/pageobjects/admin.page.js
+++ test/pageobjects/admin.page.js
@@ -13,36 +13,86 @@ class AdminPage {
     get errorContainerClient() { return $('form > div:nth-child(2) > p')}
     get activeCheckbox() { return $('div.input_checkboxContainer__3zrAz > input')}
 
+    //inputs
 
+    get inputName() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > div > form > div:nth-child(1) > input')}
+    get inputClient() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > div > form > div:nth-child(2) > input')}
+    get inputStartDate() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > div > form > div:nth-child(3) > input')}
+    get inputEndDate() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > div > form > div:nth-child(4) > input')}
+    get inputDescription() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > div > form > div.textarea_textareaContainer__zy-Jg > textarea')}
+
+
+    get changeName() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > form > div > div.form_divcontainer__35TLE > div:nth-child(1) > input')}
+    get changeClient() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > form > div > div.form_divcontainer__35TLE > div:nth-child(2) > input')}
+    get changeStartDate() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > form > div > div.form_divcontainer__35TLE > div:nth-child(4) > input')}
+    get changeEndDate() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > form > div > div.form_divcontainer__35TLE > div:nth-child(5) > input')}
+    get changeDescription() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > form > div > div.form_larger__3VW_g > div.textarea_textareaContainer__zy-Jg > textarea')}
+
+    get crossBtnEdit() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > div > div.errorsuccessmodal_container__JBOhN > div > div > img')}
+    //Edit
+    get editBtn() { return $('div > table > tbody > tr:nth-child(6) > td:nth-child(8) > button')}
+    get editForm() { return $('div.modal-form_container__A3c7w > div')}
+    get editAcceptBtn() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > div > button')}
+    get noChangesModal() { return $('div.modal-form_container__A3c7w > div > div.modal-form_form__1d3ca.modal-form_noselect__1pLwl > div > div > div')}
+
+    //Delete
+    get deleteBtn() { return $('div > table > tbody > tr:nth-child(13) > td:nth-child(9) > button')}
+    get deleteModal() { return $('div.confirmModal_modalOvelay___5vol > div')}
+    get closeDeleteBtn() { return $('div.confirmModal_modalButton__3CcD9 > button:nth-child(2)')}
+    get confirmDeleteBtn() { return $('div.confirmModal_modalOvelay___5vol > div > div.confirmModal_modalButton__3CcD9 > button:nth-child(1)')}
 
     //Setters
-    async setClient(client) {
-        await this.inputclient.setValue(client);
-    }
+
+    //Add setter
     async setName(name) {
         await this.inputName.setValue(name);
+    }
+    async setClient(client) {
+        await this.inputClient.setValue(client);
     }
     async setStartDate(startDate) {
         await this.inputStartDate.setValue(startDate);
     }
-    async setEndDAte(endDAte) {
-        await this.inputEndDAte.setValue(endDAte);
+    async setEndDate(endDate) {
+        await this.inputEndDate.setValue(endDate);
     }
     async setDescription(description) {
         await this.inputDescription.setValue(description);
     }
-    async setActive(active) {
-        await this.inputActive.setValue(active);
+
+    //Edit setter
+    async editName(name) {
+        await this.changeName.setValue(name);
+    }
+    async editClient(client) {
+        await this.changeClient.setValue(client);
+    }
+    async editStartDate(startDate) {
+        await this.changeStartDate.setValue(startDate);
+    }
+    async editEndDate(endDate) {
+        await this.changeEndDate.setValue(endDate);
+    }
+    async editDescription(description) {
+        await this.changeDescription.setValue(description);
     }
     //Methods
-    async create (name, client, startDate, endDate, description, active) {
-        await this.setName(name),
+    async create (name, client, startDate, endDate, description) {
+        await this.setName(name);
         await this.setClient(client);
         await this.setStartDate(startDate);
-        await this.setEndDAte(endDate);
+        await this.setEndDate(endDate);
         await this.setDescription(description);
-        await this.activeCheckbox.click();
+        await this.activeCheckbox.click()
         await this.createBtn.click();
+    }
+    async edit (name, client, startDate, endDate, description) {
+        await this.editName(name),
+        await this.editClient(client);
+        await this.editStartDate(startDate);
+        await this.editEndDate(endDate);
+        await this.editDescription(description);
+        await this.editAcceptBtn.click();
     }
     open () {
         return browser.url('https://alexis-trackgenix-app.vercel.app/home');
--- test/specs/admin.spec.js
+++ test/specs/admin.spec.js
@@ -1,37 +1,73 @@
 const AdminPage = require('../pageobjects/admin.page')
 
 describe('Login page testing', () => {
-
+    beforeAll('open browser', ()=> {
+        browser.url('https://alexis-trackgenix-app.vercel.app/home')
+    })
 
     describe('Admin user', () => {
         it('Click on admin button', async () => {
-            browser.url('https://alexis-trackgenix-app.vercel.app/home')
-            await AdminPage.adminUser.click()
-            await expect(browser.url('https://alexis-trackgenix-app.vercel.app/admin'))
+            await AdminPage.adminUser.click();
+            await expect(browser).toHaveUrl('https://alexis-trackgenix-app.vercel.app/admin');
         })
         it('Click on projects', async () => {
-            await browser.pause(3000)
-            await AdminPage.projectsBtn.click()
-            await expect(browser.url('https://alexis-trackgenix-app.vercel.app/admin/projects'))
+            await AdminPage.projectsBtn.click();
+            await expect(browser).toHaveUrl('https://alexis-trackgenix-app.vercel.app/admin/projects');
         })
-        it('Click on projects title', async () => {
+        it('Add button should exist', async () => {
             await expect(AdminPage.addProject).toExist();
         })
     })
     describe('Add projects', () => {
         it('Click on add project', async () => {
-            await AdminPage.addProject.click()
-            await expect(AdminPage.projectForm).toExist()
+            await AdminPage.addProject.click();
+            await expect(AdminPage.projectForm).toExist();
         })
         it('Click on create project without data', async () => {
-            await AdminPage.addProject.click()
-            await expect(AdminPage.errorContainerClient).toHaveText('Client name is a required field')
+            await AdminPage.createBtn.click();
+            await expect(AdminPage.errorContainerClient).toHaveText('Client name is a required field');
         })
         it('Click on create project with valid data', async () => {
-            await AdminPage.create('Charlie', 'Parker', '29/08/1920', '12/03/1955', 'Crazy saxophonist from the bebop era',  )
-            await expect(AdminPage.errorContainerClient).toHaveText('Client name is a required field')
+            await AdminPage.create('Charlie', 'Parker', '29/08/1920', '12/03/1955', 'Crazy saxophonist from the bebop era');
         })
-
+        it('Exit create form', async () => {
+            await AdminPage.crossBtnEdit.click();
+            await expect(AdminPage.editBtn).toExist();
+        })
+    })
+    describe('Edit projects', () => {
+        it('Click on edit project', async () =>{
+            await AdminPage.editBtn.click();
+            await expect(AdminPage.editForm).toExist();
+        })
+        it('Click on edit withoud data', async () =>{
+            await AdminPage.editAcceptBtn.click();
+            await expect(AdminPage.noChangesModal).toExist();
+            await AdminPage.crossBtnEdit.click();
+        })
+        it('Edit project data', async () =>{
+            await AdminPage.editAcceptBtn.click();
+            await expect(AdminPage.noChangesModal).toExist();
+            await AdminPage.crossBtnEdit.click();
+        })
+        it('Click on create project with valid data', async () => {
+            await AdminPage.edit('Carlos', 'Solari', '17/01/1949', '12/03/1980', 'Most iconic rock leader from Argentina');
+            await broser.pause(2000)
+            await AdminPage.crossBtnEdit.click();
+        })
+    })
+    describe('Delete projects', () => {
+        it('Close button', async () =>{
+            await browser.pause(3000)
+            await AdminPage.deleteBtn.click();
+            await expect(AdminPage.deleteModal).toExist();
+            await AdminPage.closeDeleteBtn.click()
+        })
+        it('Confirm deleten', async () =>{
+            await AdminPage.deleteBtn.click();
+            await expect(AdminPage.deleteModal).toExist();
+            await AdminPage.confirmDeleteBtn.click()
+        })
     })
 
 })
